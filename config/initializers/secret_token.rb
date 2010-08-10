a7041f90c78190e3d92e1c3c162159252baf1f1c
Stefano Hug <stefano.hug@gmail.com>
2010-08-10 13:21:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orangerie::Application.config.secret_token = 'dfdaed1978c0fa6cfaee708ed9b7229bf94d40f1cd2e9dd382eb9114397602ca9afffb76393d95ac4570cde0f35804e191b53c8e908fa77c2d02678e2629405c'
